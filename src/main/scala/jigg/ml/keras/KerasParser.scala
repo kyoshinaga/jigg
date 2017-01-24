22124e704f1699c1310d982a90b0d3fff73df491
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2017-01-24 13:11:39 +0900

    [add] KerasSsplit.

---
+++ src/main/scala/jigg/ml/keras/KerasParser.scala
@@ -0,0 +1,78 @@
+package jigg.ml.keras
+
+/*
+ Copyright 2013-2015 Hiroshi Noji
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licencses/LICENSE-2.0
+     
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitation under the License.
+*/
+
+import breeze.linalg.argmax
+import jigg.ml.keras._
+import jigg.util.LookupTable
+
+import scala.collection.mutable.ListBuffer
+
+class KerasParser(modelPath: String, tablePath: String) {
+
+  private val model = KerasModel(modelPath)
+  private val table = LookupTable(tablePath)
+
+  /*
+ * BIO tag
+ *  B : Begin of segment.               Value is 0.
+ *  I : Continuation or end of segment. Value is 1.
+ *  O : Outside of segment.             Value is 2.
+ */
+  private val tagset:Map[Int, String] = Map(0 -> "B", 1 -> "I", 2 -> "O")
+
+  def parsing(str: String): List[(Int, Int)] = {
+    // For dummy input to indicate beginning of sentence.
+    val s = "\n" + str
+    val inputData = table.encode(s)
+    val outputData = model.convert(inputData)
+
+    val tags = for {
+      i <- 1 until outputData.rows
+      maxID = argmax(outputData(i, ::))
+    } yield maxID
+
+    val ranges = ListBuffer[(Int, Int)]()
+    var bpos = -1
+
+    for(i <- tags.indices){
+      tagset(tags(i)) match{
+        case "B" =>
+          if(bpos == -1){
+            if(i > 0)
+              ranges += ((0, i))
+          }
+          else{
+            if(bpos != i)
+              ranges += ((bpos, i))
+          }
+          bpos = i
+        case "O" =>
+          ranges += ((bpos, i))
+          bpos = i + 1
+        case _ if i == tags.indices.last =>
+          ranges += ((bpos, i + 1))
+        case _ =>
+      }
+    }
+    ranges.toList
+  }
+}
+
+object KerasParser{
+  def apply(modelPath: String, tablePath: String) = new KerasParser(modelPath, tablePath)
+}
---
+++ src/main/scala/jigg/util/LookupTable.scala
@@ -0,0 +1,55 @@
+package jigg.util
+
+/*
+ Copyright 2013-2015 Hiroshi Noji
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licencses/LICENSE-2.0
+     
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitation under the License.
+*/
+
+import breeze.linalg.DenseMatrix
+import com.ibm.icu.text.Transliterator
+import org.json4s.jackson.JsonMethods
+import org.json4s.{DefaultFormats, _}
+
+class LookupTable(path: String) {
+
+  private val rawTable = JsonMethods.parse(IOUtil.openIn(path))
+
+  private val transliterator = Transliterator.getInstance("Halfwidth-Fullwidth")
+
+  implicit private val formats = DefaultFormats
+  private val tables = rawTable.extract[Map[String, Map[String, Map[String, String]]]]
+
+  private val key2id = tables("_lookup")("_key2id")
+  private val id2key = tables("_lookup")("_id2key")
+
+  def encode(str: String): DenseMatrix[Float] = {
+    val s = transliterator.transliterate(str)
+    val strArray = s.map{x =>
+      key2id.getOrElse(x.toString, "0").toFloat
+    }.toArray
+    new DenseMatrix[Float](1, str.length, strArray)
+  }
+
+  def decode(data: DenseMatrix[Float]): String = data.map{x => id2key.getOrElse(x.toInt.toString, "NONE")}.toArray.mkString("")
+
+  def getId(key: String): Int = key2id.getOrElse(key, "0").toInt
+  def getId(key: Char): Int = getId(key.toString)
+
+  def getKey(id: Int): String = id2key.getOrElse(id.toString, "UNKNOWN")
+
+}
+
+object LookupTable {
+  def apply(path: String) = new LookupTable(path)
+}
